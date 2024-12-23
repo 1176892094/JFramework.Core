a538d790b5d9c8676521334e5f771b69af0811f1
Charlotte <75679851+1176892094@users.noreply.github.com>
2024-12-23 18:30:28 +0800

    Create AssemblyInfo.cs

--- Runtime/Properties/AssemblyInfo.cs
+++ Runtime/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JFramework")]
+[assembly: AssemblyTitle("Runtime")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JFramework")]
+[assembly: AssemblyProduct("Runtime")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("24045516-3ED0-40AB-858A-13AB78FD9D03")]
+[assembly: Guid("FD012FF7-A4B6-4E21-95B9-26018DE681F4")]
 
 // Version information for an assembly consists of the following four values:
 //
